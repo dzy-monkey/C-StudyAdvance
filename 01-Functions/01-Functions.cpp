8c267078c053c1d0999cdc5188c10c48c7be2891
DIK_Tesla <362401513@qq.com>
2020-04-07 11:43:42 +0800

    项目初次提交:tada:

---
+++ 01-Functions/01-Functions.cpp
@@ -0,0 +1,28 @@
+﻿/*
+函数
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+//函数的声明
+void greet();
+int sum(int a, int b);
+
+int main()
+{
+	//调用sum函数传入实际参数
+	sum(1, 2);
+	greet();
+}
+//没有返回值，没有参数
+void greet() {
+	cout << "Hello!" << endl;
+}
+
+//返回值为int类型
+//形式参数为2个int类型的值a,b
+//函数名为sum
+int sum(int a, int b) {
+	int res = a + b;
+	return res;
+}
---
+++ 02-ActionScope/02-ActionScope.cpp
@@ -0,0 +1,32 @@
+﻿/*
+作用域
+*/
+
+#include <iostream>
+using namespace std;
+int min(int num1, int num2, int num3);
+//全局变量，作用域是全局的，不管在哪个函数中都可以调用
+int a = 3;
+int main()
+{
+	//res的作用域为main函数
+	int res = min(3, 9, 2);
+	res = min(2, 3, 5);
+	cout << res << endl;
+	cout << a << endl;
+}
+//形参的作用域只在所定义的函数内部
+//块执行期间的对象称为自动对象
+int min(int num1, int num2, int num3) {
+	//res 的作用域为min函数
+	int res;
+	//静态局部变量，直到程序终止才被销毁
+	static int count = 0;
+	count+=1;
+	cout << "count:" << count << endl;
+	res = num1 < num2 ? num1 : num2;
+	res = res < num3 ? res : num3;
+	//测试全局变量
+	cout << a << endl;
+	return res;
+}
